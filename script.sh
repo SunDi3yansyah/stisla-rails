5d4cfe6b92f316fd1a7fcc253f5fa3d8b0ac28f9
Cahyadi Triyansyah <sundi3yansyah@gmail.com>
2019-02-11 16:28:13 +0700

    add script

---
+++ script.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+cp -R stisla/sources/scss/* vendor/assets/stylesheets
+cp -R stisla/dist/assets/js/* vendor/assets/javascripts
+cp -R stisla/dist/assets/fonts/* vendor/assets/fonts
+cp -R stisla/dist/assets/img/* vendor/assets/img
+cp -R stisla/dist/assets/modules/* vendor/assets/modules # while taking it from here first
+cp vendor/assets/modules/fontawesome/css/all.css vendor/assets/modules/fontawesome/css/all.scss
+
+sed -i "s|url('../img/|asset-url('|g" vendor/assets/stylesheets/**/*.scss
+sed -i 's|url("../img/|asset-url("|g' vendor/assets/stylesheets/**/*.scss
+
+sed -i "s|url('../fonts/|asset-url('|g" vendor/assets/stylesheets/**/*.scss
+sed -i 's|url("../fonts/|asset-url("|g' vendor/assets/stylesheets/**/*.scss
+
+sed -i 's|url("..|asset-url("fontawesome|g' vendor/assets/modules/fontawesome/css/all.scss
