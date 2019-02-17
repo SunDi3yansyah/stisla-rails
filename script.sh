87f7cea493a04540355f2c52775b8dcc8c3cc270
Cahyadi Triyansyah <sundi3yansyah@gmail.com>
2019-02-17 22:26:46 +0700

    fix fontawesome

--- script.sh
+++ script.sh
@@ -7,7 +7,8 @@ cp -R stisla/dist/assets/js/* vendor/assets/javascripts
 cp -R stisla/dist/assets/fonts/* vendor/assets/fonts
 cp -R stisla/dist/assets/img/* vendor/assets/img
 cp -R stisla/dist/assets/modules/* vendor/assets/modules # while taking it from here first
-cp vendor/assets/modules/fontawesome/css/all.css vendor/assets/modules/fontawesome/css/all.scss
+mv vendor/assets/modules/fontawesome/css/all.css vendor/assets/modules/fontawesome/css/all.scss
+mv vendor/assets/modules/fontawesome/css/all.min.css vendor/assets/modules/fontawesome/css/all.min.scss
 
 sed -i "s|url('../img/|asset-url('|g" vendor/assets/stylesheets/**/*.scss
 sed -i 's|url("../img/|asset-url("|g' vendor/assets/stylesheets/**/*.scss
@@ -15,6 +16,6 @@ sed -i 's|url("../img/|asset-url("|g' vendor/assets/stylesheets/**/*.scss
 sed -i "s|url('../fonts/|asset-url('|g" vendor/assets/stylesheets/**/*.scss
 sed -i 's|url("../fonts/|asset-url("|g' vendor/assets/stylesheets/**/*.scss
 
-sed -i 's|url("..|asset-url("fontawesome|g' vendor/assets/modules/fontawesome/css/all.scss
+sed -i 's|url("..|asset-url("fontawesome|g' vendor/assets/modules/fontawesome/css/*.scss
 
 echo "Yay! copying modules from Stisla was successfully done"
